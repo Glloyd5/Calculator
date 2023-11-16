248cc21a553537b20525d17c3e91246c62485c30
Griffin <80220535+Glloyd5@users.noreply.github.com>
2023-11-16 14:25:44 -0700

    Add html structure and css stylings for calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function(a, b){
+    let evaluated = (a) + (b);
+    return evaluated;
+}
+
+const subtract = function(a, b){
+    let evaluated = (a) - (b);
+    return evaluated;
+}
+
+const multiply = function(a, b){
+    let evaluated = (a) * (b);
+    return evaluated;
+}
+
+const divide = function(a, b){
+    let evaluated = (a) / (b);
+    return evaluated;
+}
+
