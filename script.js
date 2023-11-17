dae596075c17a12f7f190de868c8c6b02a72a769
Griffin <80220535+Glloyd5@users.noreply.github.com>
2023-11-17 13:45:12 -0700

    Add calculator functions and script foundation.

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-const add = function(a, b){
-    let evaluated = (a) + (b);
-    return evaluated;
+let firstNum;
+let operator;
+let secondNum;
+let result;
+let outputResult = document.querySelector('#result');
+let outputExpression = document.querySelector('#expression');
+let digitButtons = document.querySelectorAll('.digitB');
+let operatorButtons = document.querySelectorAll('.operatorB');
+
+digitButtons.addEventListener('click',)
+operatorButtons.addEventListener('click',)
+
+const updateExpression = function(){
+    outputExpression.textContent = `${firstNum} ${operator} ${secondNum}`;
 }
 
-const subtract = function(a, b){
-    let evaluated = (a) - (b);
-    return evaluated;
+const updateResult = function(){
+    outputResult.textContent = `${result}`;
 }
 
-const multiply = function(a, b){
-    let evaluated = (a) * (b);
-    return evaluated;
-}
+const add = function (firstNum, secondNum) {
+  result = firstNum + secondNum;
+};
 
-const divide = function(a, b){
-    let evaluated = (a) / (b);
-    return evaluated;
-}
+const subtract = function (firstNum, secondNum) {
+  result = firstNum - secondNum;
+};
 
+const multiply = function (firstNum, secondNum) {
+  result = firstNum * secondNum;
+};
+
+const divide = function (firstNum, secondNum) {
+  result = firstNum / secondNum;
+};
+
+const evaluate = function (firstNum, secondNum) {
+  if (operator == 'add') {
+    add(firstNum, secondNum);
+  } else if (operator == 'subtract') {
+    subtract(firstNum, secondNum);
+  } else if (operator == 'multiply') {
+    multiply(firstNum, secondNum);
+  } else if (operator == 'divide') {
+    divide(firstNum, secondNum);
+  }
+};
