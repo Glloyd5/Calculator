d6c1bcfa69140ec0f7d2fcc1e443b71158887d75
Griffin <80220535+Glloyd5@users.noreply.github.com>
2023-11-24 18:25:29 -0700

    Add css new background img calc js func and html

--- script.js
+++ script.js
@@ -1,46 +1,73 @@
 let firstNum;
 let operator;
 let secondNum;
-let result;
+let result = 0;
 let outputResult = document.querySelector('#result');
 let outputExpression = document.querySelector('#expression');
 let digitButtons = document.querySelectorAll('#digits');
 let operatorButtons = document.querySelectorAll('#operators');
-let displayValue = ""
+let displayValue = '';
 
-const updateDisplay = function(e){
-  if (e.value != '+' && e.value != '*' && e.value != '/' && e.value != '-'){
-    displayValue += e.value
-    outputExpression.textcontent = displayValue;
+const updateDisplay = function (e) {
+  if (e.value != '+' && e.value != '*' && e.value != '/' && e.value != '-') {
+    displayValue += e.value;
+  } else if (
+    e.value == '+' ||
+    e.value == '*' ||
+    e.value == '/' ||
+    e.value == '-'
+  ) {
+    if (
+      displayValue.includes('+') ||
+      displayValue.includes('-') ||
+      displayValue.includes('*') ||
+      displayValue.includes('/')
+    ) {
+      if (result != 0) {
+        displayValue = result + ` ${e.value} `;
+      }
+    } else if (displayValue.length < 1) {
+      displayValue = `0 ${e.value} `;
+    } else {
+      displayValue += ` ${e.value} `;
+    }
   }
-  else if (e.value == '+' || e.value == '*' || e.value == '/' || e.value == '-'){
-    if(displayValue.includes('+') || displayValue.includes('-') || displayValue.includes('*') || displayValue.includes('/')){
-      
-    } else {displayValue += ` ${e.value} `;}
-    outputExpression.textcontent = displayValue;
-  }
-  document.getElementById("expression").textContent = displayValue;
-}
+  document.getElementById('expression').textContent = displayValue;
+};
 
-document.querySelectorAll('.digitB').forEach(el => el.addEventListener('click', () => {
-  updateDisplay(el);
-}));
+document.querySelectorAll('.digitB').forEach((el) =>
+  el.addEventListener('click', () => {
+    updateDisplay(el);
+  })
+);
 
-document.querySelectorAll('.operatorB').forEach(el => el.addEventListener('click', () => {
-  updateDisplay(el);
-}));
+document.querySelectorAll('.operatorB').forEach((el) =>
+  el.addEventListener('click', () => {
+    updateDisplay(el);
+  })
+);
+
+document.querySelector('#clear').addEventListener('click', () => {
+  clear();
+});
 
 document.querySelector('#evaluate').addEventListener('click', () => {
-  let splitExpression = displayValue.split(" ", 3)
+  let splitExpression = displayValue.split(' ', 3);
   console.log(splitExpression);
   firstNum = Number(splitExpression[0]);
   operator = splitExpression[1];
   secondNum = Number(splitExpression[2]);
   evaluate(firstNum, splitExpression[1], secondNum);
-  document.getElementById("result").textContent = result;
+  document.getElementById('result').textContent = result;
 });
 
-
+const clear = function () {
+  result = 0;
+  document.getElementById('result').textContent = 0;
+  displayValue = '';
+  document.getElementById('expression').textContent = '';
+  console.log('hi');
+};
 
 const evaluate = function (firstNum, operator, secondNum) {
   if (operator == '+') {
@@ -50,6 +77,15 @@ const evaluate = function (firstNum, operator, secondNum) {
   } else if (operator == '*') {
     result = firstNum * secondNum;
   } else if (operator == '/') {
-    result = firstNum / secondNum;
+    if (secondNum == 0) {
+      result = "Error";
+      document.getElementById('result').textContent = result;
+      setTimeout(() => {clear()}, 3000); 
+    } else {
+      result = firstNum / secondNum;
+    }
   }
 };
+
+// Fix dont allow numbers to be added to display value after a calculation
+// Fix if display value is 0 and they use a 2nd operator calculate.
